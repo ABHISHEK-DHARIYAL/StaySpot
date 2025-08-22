25eafb8af8a55a8fd9c192d2999931e1b23faff7
ABHISHEK-DHARIYAL <dhariyalabhi@gmail.com>
2025-08-22 11:52:22 +0530

    loader modify

--- public/js/script.js
+++ public/js/script.js
@@ -15,30 +15,3 @@
     );
   });
 })();
-
-let loaderTimeout;
-document.addEventListener("DOMContentLoaded", () => {
-  loaderTimeout = setTimeout(() => {
-    showLoader();
-  }, 100);
-});
-window.addEventListener("load", () => {
-  clearTimeout(loaderTimeout);
-  hideLoader();
-});
-function showLoader() {
-  const loader = document.getElementById("loader");
-  if (loader) {
-    loader.style.display = "flex";
-    loader.style.opacity = "1";
-  }
-}
-function hideLoader() {
-  const loader = document.getElementById("loader");
-  if (loader) {
-    loader.style.opacity = "0";
-    setTimeout(() => {
-      loader.style.display = "none";
-    }, 300);
-  }
-}
